15844bdbceb74add3d55d6949ce272f4fcc95e07
andrew-savich <savichandrew@Gmail.com>
2021-11-22 13:59:12 +0300

    Chnged Employee base url

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,35 +1,35 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/employee-manager/api/employee";
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees/";
 
 class EmployeeService {
 
     getEmployeeList() {
-        return axios.get(EMPLOYEE_API_BASE_URL + '/all');
+        return axios.get(EMPLOYEE_API_BASE_URL);
     }
 
     createEmployee(employee){
-        return axios.post(EMPLOYEE_API_BASE_URL + '/create', employee);
+        return axios.post(EMPLOYEE_API_BASE_URL, employee);
     }
 
     updateEmployee(employee){
-        return axios.put(EMPLOYEE_API_BASE_URL + '/update', employee);
+        return axios.put(EMPLOYEE_API_BASE_URL, employee);
     }
 
     deleteEmployee(employeeId){
-        return axios.delete(EMPLOYEE_API_BASE_URL + '/delete/' + employeeId);
+        return axios.delete(EMPLOYEE_API_BASE_URL + employeeId);
     }
 
     getEmployeeById(employeeId){
-        return axios.get(EMPLOYEE_API_BASE_URL + '/get/' + employeeId);
+        return axios.get(EMPLOYEE_API_BASE_URL + employeeId);
     }
 
     getEmployeeGenders(){
-        return axios.get(EMPLOYEE_API_BASE_URL + '/genders');
+        return axios.get(EMPLOYEE_API_BASE_URL + 'genders');
     }
 
     getEmployeeDepartments(){
-        return axios.get(EMPLOYEE_API_BASE_URL + '/departments');
+        return axios.get(EMPLOYEE_API_BASE_URL + 'departments');
     }
 
 }
